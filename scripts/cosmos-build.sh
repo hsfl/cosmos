482ba12ad17fd15ae915627f7e43105869b7d9fc
Miguel Nunes <spacemig@gmail.com>
2022-03-25 00:31:28 -1000

    Update cosmos-build.sh

--- scripts/cosmos-build.sh
+++ scripts/cosmos-build.sh
@@ -41,10 +41,10 @@ fi
 
 if [ "$verbose" = "no" ]; then
 	cmake $cosmosFolder/source/core &>cmake.log
-	make -j2 install &>install.log
+	make -j4 install &>install.log
 else
 	cmake $cosmosFolder/source/core >cmake.log
-	make -j2 install >install.log
+	make -j4 install >install.log
 fi
 
 echo "COSMOS Build done!"
